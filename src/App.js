b6651356b0bd43b7397836b64cc14f1095e0750a
keith <xqk559@gmail.com>
2020-03-12 10:07:55 -0400

    Console logged out App.js JSX and started replacing with custom burger for CSS practice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+import classes from './burger.module.css';
 import Navigation from './components/Nav/Navigation';
 import ProductsPage from './containers/Products';
 import FavoritesPage from './containers/Favorites';
 
 const App = props => {
   return (
-    <React.Fragment>
-      <Navigation />
-      <main>
-        <Route path="/" component={ProductsPage} exact />
-        <Route path="/favorites" component={FavoritesPage} />
-      </main>
-    </React.Fragment>
+    <div className = {classes.Box}>&nbsp;
+      <div className = {classes.Breadtop}>&nbsp;</div>
+    </div>
   );
+    // <React.Fragment>
+    //   <Navigation />
+    //   <main>
+    //     <Route path="/" component={ProductsPage} exact />
+    //     <Route path="/favorites" component={FavoritesPage} />
+    //   </main>
+    // </React.Fragment>
+  //);
 };
 
 export default App;
